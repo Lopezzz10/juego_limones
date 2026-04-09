5500ef1594d40546e5c7cdf279143c9a7ef92f17
Lopezzz10 <josemanuellopezolivez@gmail.com>
2026-04-09 13:57:05 -0500

    AGREGAR dibujarSuelo Y dibujarPersonaje

--- limones.js
+++ limones.js
@@ -1,6 +1,8 @@
 let canvas = document.getElementById("areaJuego")
 let ctx=canvas.getContext("2d")
 const ALTURA_SUELO = 40
+const ALTURA_PERSONAJE = 60
+const ANCHO_PERSONAJE = 40
 
 function iniciar(){
     dibujarSuelo()
@@ -13,5 +15,5 @@ function dibujarSuelo(){
 
 function dibujarPersonaje(){
     ctx.fillStyle="yellow"
-    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+60),40,60);
+    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
 }
